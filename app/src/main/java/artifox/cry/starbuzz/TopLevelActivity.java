b0fe5c93ab5a0858704b54855e43b9dd327197ba
Artsiom Koptsiukh <artsiom.koptsiukh@gmail.com>
2016-07-16 19:22:25 +0300

    TopLevelActivity final v1

--- app/src/main/java/artifox/cry/starbuzz/TopLevelActivity.java
+++ app/src/main/java/artifox/cry/starbuzz/TopLevelActivity.java
@@ -1,13 +1,29 @@
 package artifox.cry.starbuzz;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class TopLevelActivity extends AppCompatActivity {
+    private ListView
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+
+        ListView listView = (ListView) findViewById(R.id.list_options);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
